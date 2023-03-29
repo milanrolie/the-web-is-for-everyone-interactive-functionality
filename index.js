9d8cb4dd9f85e7b8ed1e7147e4230e9e694e6c88
milanrolie <112857190+milanrolie@users.noreply.github.com>
2023-03-29 13:56:24 +0200

    wejo

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express'
+
 const app = express()
 
 
@@ -11,23 +12,67 @@ app.set('port', process.env.PORT || 4000)
 app.listen(app.get('port'), function () {
 })
 
+
 const url = ["https://raw.githubusercontent.com/fdnd-agency/ultitv/main/ultitv-api"];
+const postUrl = "https://api.ultitv.fdnd.nl/api/v1/players";
+const apiUrl = "https://api.ultitv.fdnd.nl/api/v1/questions";
 
 
 const urls = [
-  [url] + "/game/943.json",
-  [url] + "/game/943/statistics.json",
-  [url] + "/facts/Player/8607.json",
+  url + "/game/943.json",
+  url + "/game/943/statistics.json",
+  url + "/facts/Player/8607.json",
+  postUrl,
+  apiUrl
 ];
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", async function (request, response) {
-  const [data1, data2, data3] = await Promise.all(urls.map(fetchJson));
-  const data = { data1, data2 };
+  const [data1, data2, data3, data4, data5] = await Promise.all(urls.map(fetchJson));
+  const data = { data1, data2, data3, data4, data5 };
   response.render("index", data);
 });
 
-async function fetchJson(url) {
-  return await fetch(url)
+app.post("/", (request, response) => {
+
+  // console.log(request.body)
+  request.body.answers = [
+    {
+      content: request.body.content,
+      questionId: request.body.question
+    }
+  ];
+
+  console.log(request.body.answers);
+
+
+  postJson(postUrl, request.body).then((data) => {
+    let newPlayer = { ...request.body };
+    if (data.success) {
+      response.redirect("/?memberPosted=true");
+    } else {
+      const errormessage = `${data.message}: De lul.`;
+      const newplayer = { error: errormessage, values: newPlayer };
+      console.log(errormessage);
+      response.redirect("/");
+    }
+  });
+});
+
+async function fetchJson(urls) {
+  return await fetch(urls)
+    .then((response) => response.json())
+    .catch((error) => error)
+}
+
+async function postJson(urls, body) {
+  return await fetch(urls, {
+    method: 'post',
+    body: JSON.stringify(body),
+    headers: { 'Content-Type': 'application/json' },
+  })
     .then((response) => response.json())
     .catch((error) => error)
 }
